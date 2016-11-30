d5abada9cd2a9929a2669d9f4ec53081c68aeed0
Bloke <thatbloke@stefdawson.com>
2016-11-30 11:55:50 +0000

    Allow Textile 3.5+ to be used in compilation
    Also, convert to spaces for indentation.

--- zem_tpl.php
+++ zem_tpl.php
@@ -1,62 +1,72 @@
 <?php
 
-// Either copy classTextile.php to your plugin directory, or uncomment the following
-// line and edit it to give the location where classTextile.php can be found
+// Either:
+//  a) Copy a full single-file version of classTextile.php to your plugin directory.
+//  b) Uncomment the following line and edit it to give the location where
+//     classTextile.php can be found
 #ini_set('include_path', ini_get('include_path') . ':/full/path/to/textile');
+//  c) Uncomment the following line and edit it to give the location of the textpattern
+//     directory inside a nearby, full Textpattern installation.
+#define('txpath', '/full/path/to/textpattern');
 
 if (empty($test)) {
-	exit(compile_plugin());
+    exit(compile_plugin());
 }
 
 // -----------------------------------------------------
 
 function extract_section($lines, $section) {
-	$result = "";
-	
-	$start_delim = "# --- BEGIN PLUGIN $section ---";
-	$end_delim = "# --- END PLUGIN $section ---";
+    $result = "";
 
-	$start = array_search($start_delim, $lines) + 1;
-	$end = array_search($end_delim, $lines);
+    $start_delim = "# --- BEGIN PLUGIN $section ---";
+    $end_delim = "# --- END PLUGIN $section ---";
 
-	$content = array_slice($lines, $start, $end-$start);
+    $start = array_search($start_delim, $lines) + 1;
+    $end = array_search($end_delim, $lines);
 
-	return join("\n", $content);
+    $content = array_slice($lines, $start, $end-$start);
 
+    return join("\n", $content);
 }
 
 function compile_plugin($file='') {
-	global $plugin;
+    global $plugin;
 
-	if (empty($file))
-		$file = $_SERVER['SCRIPT_FILENAME'];
+    if (empty($file))
+        $file = $_SERVER['SCRIPT_FILENAME'];
 
-	if (!isset($plugin['name'])) {
-		$plugin['name'] = basename($file, '.php');
-	}
+    if (!isset($plugin['name'])) {
+        $plugin['name'] = basename($file, '.php');
+    }
 
-	# Read the contents of this file, and strip line ends
-	$content = file($file);
-	for ($i=0; $i < count($content); $i++) {
-		$content[$i] = rtrim($content[$i]);
-	}
+    // Read the contents of this file, and strip line ends.
+    $content = file($file);
+    for ($i=0; $i < count($content); $i++) {
+        $content[$i] = rtrim($content[$i]);
+    }
 
-	$plugin['help'] = trim(extract_section($content, 'HELP'));
-	$plugin['code'] = extract_section($content, 'CODE');
+    $plugin['help'] = trim(extract_section($content, 'HELP'));
+    $plugin['code'] = extract_section($content, 'CODE');
 
-	// textpattern will textile it, and encode html
-	$plugin['help_raw'] = $plugin['help'];
+    // Textpattern will textile it, and encode html.
+    $plugin['help_raw'] = $plugin['help'];
 
-	// This is for bc; and for help that needs to use
-	@include('classTextile.php');
-	if (class_exists('Textile')) {
-		$textile = new Textile();
-		$plugin['help'] = $textile->TextileThis($plugin['help']);
-	}
+    // This is for bc; and for help that needs to use it.
+    @include('classTextile.php');
 
-	$plugin['md5'] = md5( $plugin['code'] );
+    if (defined('txpath')) {
+        $loader = new \Textpattern\Loader(txpath.'/lib');
+        $loader->register();
+    }
 
-	$header = <<<EOF
+    if (class_exists('Textile')) {
+        $textile = new Textile();
+        $plugin['help'] = $textile->TextileThis($plugin['help']);
+    }
+
+    $plugin['md5'] = md5( $plugin['code'] );
+
+    $header = <<<EOF
 # {$plugin['name']} v{$plugin['version']}
 # {$plugin['description']}
 # {$plugin['author']}
@@ -69,12 +79,12 @@ function compile_plugin($file='') {
 # ......................................................................
 EOF;
 
-	$body = trim(chunk_split(base64_encode(gzencode(serialize($plugin))), 72));
+    $body = trim(chunk_split(base64_encode(gzencode(serialize($plugin))), 72));
 
-	// to produce a copy of the plugin for distribution, load this file in a browser. 
-	header('Content-type: text/plain');
+    // To produce a copy of the plugin for distribution, load this file in a browser.
+    header('Content-type: text/plain');
 
-	return $header."\n\n".$body;
+    return $header."\n\n".$body;
 }
 
 ?>
